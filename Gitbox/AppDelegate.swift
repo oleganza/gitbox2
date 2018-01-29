5217dfc801ce7d6cef1ee38243df7ab1c0cfbce0
Oleg Andreev <oleganza@gmail.com>
2018-01-28 20:45:08 -0800

    Initial Commit

---
+++ Gitbox/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Gitbox
+//
+//  Created by Oleg Andreev on 28/Jan/18.
+//  Copyright © 2018 Oleg Andreev. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Gitbox/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Gitbox
+//
+//  Created by Oleg Andreev on 28/Jan/18.
+//  Copyright © 2018 Oleg Andreev. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
